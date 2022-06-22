f6ce67815b9f3afe0383fb04e5a246116ecfb88f
jrmilholland <48189427+jrmilholland@users.noreply.github.com>
2022-06-22 13:06:21 -0700

    update bucket with state like options (#5)

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -10,6 +10,19 @@
 resource "aws_s3_bucket" "tf_course" {
 
   bucket = "hella-buckets-of-fun-some-new-name-buh"
+    # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 # resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
