42289075c95ea3de32623e280638ebf0307b7270
jrmilholland <48189427+jrmilholland@users.noreply.github.com>
2022-06-23 08:49:55 -0700

    enable dynamo (#11)
    * enable dynamo
    
    * v3 checkout

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -10,7 +10,7 @@ terraform {
 
   backend "s3" {
     bucket         = "jm-terraform-state-demo"
-#    dynamodb_table = "terraform-state-lock-dev-dynamo" // Cannot be a variable, comment before dynamo is created
+    dynamodb_table = "terraform-state-lock-dev-dynamo" // Cannot be a variable, comment before dynamo is created
     key            = "dev/us-west-2/terraform.tfstate"
     region         = "us-west-2"
     encrypt        = true
@@ -20,18 +20,6 @@ terraform {
 provider "aws" {
   region = var.region
 }
-
-# resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-#   name           = "terraform-state-lock-dev-dynamo"
-#   hash_key       = "LockID"
-#   read_capacity  = 20
-#   write_capacity = 20
-
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
 
 module "dynamo_state_lock" {
   source                   = "../../modules/dynamo_state_lock"
